b1f851ef559a060d331794f42a680486a179e9a3
Santhosh <nodegeek@gmail.com>
2014-04-22 23:14:19 +0530

    dev

--- server.js
+++ server.js
@@ -32,7 +32,8 @@ var router = express.Router(); 				// get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+    d = "hello world"
+    res.send(d);
 });
 
 router.route('/news')
